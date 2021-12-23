683f50c65d17dc399a8fb85da2ec598642926bca
will-dantas <wmdantas2@gmail.com>
2021-12-22 22:48:08 -0300

    home ig.news

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,26 @@
 import Head from 'next/head';
 
-import styles from '../styles/home.module.scss'
+import styles from './home.module.scss'
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Inicío | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
-      <h1 className={styles.title}>
-        Hello world
-      </h1>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world</h1>
+          <p>
+            Get access to all publications <br/>
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+
+        <img src="/images/avatar.svg" alt="coding" />
+      </main>
     </>
   )
 }
