fe35a441abaee6c333e531d9538788860c7a739c
will-dantas <wmdantas2@gmail.com>
2022-01-10 19:00:54 -0300

    Format posts

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,33 +1,37 @@
 import { GetStaticProps } from 'next';
-import Prismic from '@prismicio/client'; 
+import Prismic from '@prismicio/client';
 import Head from 'next/head';
 import styles from './styles.module.scss'
 import { getPrismicClient } from '../../services/prismic';
+import { RichText } from 'prismic-dom'
 
-export default function Posts() {
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+}
+
+interface PostsProps {
+  posts: Post[]
+}
+
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <Head>
-        <title>Posts | Ignews</title> 
+        <title>Posts | Ignews</title>
       </Head>
 
       <main className={styles.container}>
-        <div className={styles.posts }>
-          <a href="">
-            <time>9 de janeiro de 2022</time>
-            <strong>Titulo</strong>
-            <p>exceto para obter alguma vantagem com isso? Mas quem tem o direito de criticar um homem que escolhe desfrutar de um prazer que não tem consequências desagradáveis, ou aquele que evita uma dor que não produz o prazer resultante?</p>
-          </a>
-          <a href="">
-            <time>9 de janeiro de 2022</time>
-            <strong>Titulo</strong>
-            <p>exceto para obter alguma vantagem com isso? Mas quem tem o direito de criticar um homem que escolhe desfrutar de um prazer que não tem consequências desagradáveis, ou aquele que evita uma dor que não produz o prazer resultante?</p>
-          </a>
-          <a href="">
-            <time>9 de janeiro de 2022</time>
-            <strong>Titulo</strong>
-            <p>exceto para obter alguma vantagem com isso? Mas quem tem o direito de criticar um homem que escolhe desfrutar de um prazer que não tem consequências desagradáveis, ou aquele que evita uma dor que não produz o prazer resultante?</p>
-          </a>
+        <div className={styles.posts}>
+          {posts.map(post => (
+            <a key={post.slug} href="#">
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          ))}
         </div>
       </main>
     </>
@@ -41,12 +45,25 @@ export const getStaticProps: GetStaticProps = async () => {
     Prismic.predicates.at('document.type', 'post')
   ], {
     fetch: ['post.title', 'post.content'],
-    pageSize: 100, 
+    pageSize: 100,
   })
-  console.log(JSON.stringify(response, null, 2))
+
+  const posts = response.results.map(post => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
+      })
+
+    };
+  });
 
   return {
-    props: {}
+    props: { posts }
   }
 
 }
